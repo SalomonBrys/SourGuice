2679130f8d22e76684bb066973028c75d98c76f1
Salomon BRYS <salomon.brys@weemo.com>
2014-04-19 01:01:54 +0200

    Small move in WS

--- sourguice-ws/src/main/java/com/github/sourguice/ws/WSServlet.java
+++ sourguice-ws/src/main/java/com/github/sourguice/ws/WSServlet.java
@@ -1,6 +1,7 @@
 package com.github.sourguice.ws;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,18 +18,42 @@ import com.google.gson.Gson;
 @SuppressWarnings("serial")
 public class WSServlet extends HttpServlet {
 
+	public static final String REST = "/rest/";
+
 	private final Map<String, TypedProvider<?>> controllers = new HashMap<>();
 
 	public final WSDescription description = new WSDescription();
 
-	public void add(final TypedProvider<?> controler) {
-		this.controllers.put(controler.getTypeLiteral().getRawType().getName(), controler);
+	public void add(final TypedProvider<?> controller) {
+		this.controllers.put(controller.getTypeLiteral().getRawType().getName(), controller);
+	}
+
+	private void doRest(final String pathInfo, final HttpServletRequest req, final HttpServletResponse res) throws IOException {
+		res.getWriter().write("REST");
+	}
+
+	private void doExplorer(final HttpServletRequest req, final HttpServletResponse res) throws IOException {
+		res.getWriter().write("Explorer");
 	}
 
 	@Override
 	protected void doGet(final HttpServletRequest req, final HttpServletResponse res) throws ServletException, IOException {
-//		res.getWriter().write(req.getPathInfo());
-		new Gson().toJson(this.description, res.getWriter());
+		final String pathInfo = req.getPathInfo();
+
+		if (pathInfo == null || pathInfo.equals("/")) {
+			doExplorer(req, res);
+		}
+		else if (pathInfo.equals("/schema")) {
+			res.setHeader("Access-Control-Allow-Origin", "*");
+			res.setContentType("application/json");
+			new Gson().toJson(this.description, res.getWriter());
+		}
+		else if (pathInfo.startsWith(REST)) {
+			doRest(req.getPathInfo().substring(REST.length()), req, res);
+		}
+		else {
+			super.doGet(req, res);
+		}
 	}
 
 	public void initialize() {
